fca96441b60d8920317007a6f18fd10cfed53b32
Ilya Osipov <070809010@mail.ru>
2017-08-11 07:03:54 +0500

    Add auth for users and publishing

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -8,18 +9,23 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
 def post_detail(request, pk):
         post = get_object_or_404(Post, pk=pk)
         return render(request, 'blog/post_detail.html', {'post': post})
 
 
+@login_required
 def post_new(request):
         if request.method == "POST":
             form = PostForm(request.POST)
             if form.is_valid():
                 post = form.save(commit=False)
                 post.author = request.user
-                post.published_date = timezone.now()
                 post.save()
                 return redirect('post_detail', pk=post.pk)
         else:
@@ -27,6 +33,7 @@ def post_new(request):
         return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
 		post = get_object_or_404(Post, pk=pk)
 		if request.method == "POST":
@@ -34,9 +41,22 @@ def post_edit(request, pk):
 		    if form.is_valid():
 		        post = form.save(commit=False)
 		        post.author = request.user
-		        post.published_date = timezone.now()
 		        post.save()
 		        return redirect('post_detail', pk=post.pk)
 		else:
 		    form = PostForm(instance=post)
 		return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
+from django.contrib.auth import views
 from django.contrib import admin
 
 urlpatterns = [
     url(r'', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', views.login, name='login'),
 ]
