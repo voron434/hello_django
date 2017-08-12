b5293da0d91d5711b084aabb3ec7ef0275c48183
Ilya Osipov <070809010@mail.ru>
2017-08-12 08:28:30 +0500

    Add comments for posts

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.ForeignKey('auth.User')
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+def approved_comments(self):
+    return self.comments.filter(approved_comment=True)
